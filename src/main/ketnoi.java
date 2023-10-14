bc9cc441f5bc53e642e89f6ec54584bdc55cf4d3
namtalang2k3 <vunam4280@gmail.com>
2023-10-14 23:22:12 +0700

    sửa lại ket nôi database

--- src/main/ketnoi.java
+++ src/main/ketnoi.java
@@ -1,48 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author namta
- */
 public class ketnoi {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/hocform";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "";
 
-    private static String DB_URL = "jdbc:mysql://localhost:3306/qlns";
-    private static String USER_NAME = "root";
-    private static String PASSWORD = "";
-
- public static void main(String args[]) {
-        try {
-            // connnect to database 'testdb'
-            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
-            // crate statement
-            Statement stmt = conn.createStatement();
-            // get data from table 'student'
-            //ResultSet rs = stmt.executeQuery("select * from student");
-            // show data
-           
-            conn.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }    
-    public static Connection getConnection(String dbURL, String userName,
-            String password) {
+    public static Connection ketNoiDatabase() {
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(dbURL, userName, password);
-            System.out.println("connect successfully!");
-        } catch (Exception ex) {
-            System.out.println("connect failure!");
-            ex.printStackTrace();
+            // Đăng ký driver MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("Kết nối thành công đến cơ sở dữ liệu!");
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Kết nối thất bại: " + ex.getMessage());
         }
         return conn;
     }
-
-} 
+    
+    public static void main(String[] args) {
+        Connection conn = ketNoiDatabase();
+        if (conn != null) {
+            // Kết nối thành công, bạn có thể thực hiện các hoạt động với cơ sở dữ liệu ở đây
+            // Ví dụ: thực hiện truy vấn SQL
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println("Lỗi khi đóng kết nối: " + ex.getMessage());
+            }
+        } else {
+            // Xử lý khi kết nối thất bại
+            System.out.println("Không thể thực hiện hoạt động với cơ sở dữ liệu do kết nối thất bại.");
+        }
+    }
+}
